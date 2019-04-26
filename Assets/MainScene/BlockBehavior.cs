0b3b6d16252e597ef7adc6af9b50152d08365098
Michael Yu Zarng Chang <mchang22@buffalo.edu>
2019-04-26 17:32:36 -0500

    first commit

---
+++ Assets/MainScene/BlockBehavior.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBehavior : MonoBehaviour
+{
+    public Vector3 Center;
+    public Vector3 MoveDirection;
+    public bool Moving = false;
+    public float MovingSpeed = 10;
+    private float StartTime;
+    private float StartPositionOffset = 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartTime = Time.time;
+        //Init(Vector3.zero, Vector3.forward, new Vector3(7, 1, 7 ));
+    }
+    public void Init(Vector3 center, Vector3 moveDirection, Vector3 scale)
+    {
+        this.Center = center;
+        this.MoveDirection = moveDirection;
+        Moving = true;
+        transform.localScale = scale;
+        transform.position = Center - MoveDirection * StartPositionOffset;
+    }
+    // Update is called once per frame
+    public IEnumerator Recover(Vector3 recoverScale, Vector3 recoverCenter)
+    {
+        float recoverTime = Time.time;
+        Vector3 scaleDifference = recoverScale - transform.localScale;
+        Vector3 posDifference = recoverCenter - transform.position;
+        while ((Time.time - recoverTime) < 0.25f)
+        {
+            transform.localScale += scaleDifference * Time.deltaTime * 4;
+            transform.position += posDifference * Time.deltaTime * 4;
+            yield return null;
+        }
+        transform.localScale = recoverScale;
+        transform.position = recoverCenter;
+    }
+    void Update()
+    {
+        if (Moving)
+        {
+            if (MoveDirection == Vector3.forward)
+                transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong((Time.time - StartTime) * MovingSpeed, 16) - 8);
+            if (MoveDirection == Vector3.right)
+                transform.position = new Vector3(Mathf.PingPong((Time.time - StartTime) * MovingSpeed, 16) - 8, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/MainScene/CameraManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager Instance;
+    public Vector3 desirePos;
+    void Awake()
+    {
+        Instance = this;
+    }
+    void Start()
+    {
+        desirePos = transform.position;
+    }
+    public void GoUp()
+    {
+        desirePos += Vector3.up;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, desirePos, Time.deltaTime * 2);
+    }
+}
